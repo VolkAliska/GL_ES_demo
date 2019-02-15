509b3e015c99a90ee4cdb5d687c5d51ed43f375d
VolkAliska <viershtrassaliska@gmail.com>
2019-02-15 12:32:21 +0300

    Green font image

--- demo.cpp
+++ demo.cpp
@@ -263,12 +263,12 @@ int Init ( ESContext *esContext )
 
     userData->numImgIndices = 6;
 
-    for (int i = 0 ; i < 20; i++){
-        printf("\n %f", userData->imgCoords[i]);
-    }
-    for (int i = 0 ; i < 6; i++){
-        printf("\n %d", userData->imgIndices[i]);
-    }
+    // for (int i = 0 ; i < 20; i++){
+    //     printf("\n %f", userData->imgCoords[i]);
+    // }
+    // for (int i = 0 ; i < 6; i++){
+    //     printf("\n %d", userData->imgIndices[i]);
+    // }
 
     //__________________ IMAGE END ___________________
 
@@ -306,11 +306,10 @@ int Init ( ESContext *esContext )
    //  //__________________ TEXT _______________________
       
    userData->numTextIndices = 6;
-   printf("kek");
-   GLfloat y_min = -0.75f,
-            y_max = -0.35f,
-            x_min = -1.15f,
-            x_max = -0.35f;
+   GLfloat y_min = -0.80f,
+            y_max = -0.30f,
+            x_min = -1.35f,
+            x_max = -0.15f;
             
     GLuint numBlocks = 1;
     GLfloat firstBlock = x_min;
@@ -318,6 +317,7 @@ int Init ( ESContext *esContext )
     char *text = shared_I;
     //gets(text);
     numBlocks = symCount(text);
+    printf("numbl %d", numBlocks);
     
     GLfloat xlen = x_max - x_min;
     GLfloat ylen = y_max - y_min;
@@ -326,8 +326,8 @@ int Init ( ESContext *esContext )
     userData->numTextIndices *= numBlocks;
     userData-> numTextVertices = 4 * numBlocks;
 
-    GLfloat w = 1.6f; // для текстурных координат
-    
+    GLfloat w = blockLen * numBlocks; // для  координат - зависит ширина блока
+     
     GLfloat blocksVerts[userData->numTextVertices*3];
     GLfloat shiftY = (ylen - blockLen) / 2.0;
 
@@ -336,15 +336,15 @@ int Init ( ESContext *esContext )
     
     GLfloat contVert[24]=
     {
-        x_min, y_min, 0.0f,
-        x_min, y_min + shiftY, 0.0f,
-        x_max, y_min + shiftY, 0.0f,
-        x_max, y_min, 0.0f,
+        x_min, y_min, 0.004f,
+        x_min, y_min + shiftY, 0.004f,
+        x_max, y_min + shiftY, 0.004f,
+        x_max, y_min, 0.004f,
 
-        x_min, y_max - shiftY, 0.0f,
-        x_min, y_max, 0.0f,
-        x_max, y_max, 0.0f,
-        x_max, y_max - shiftY, 0.0f
+        x_min, y_max - shiftY, 0.004f,
+        x_min, y_max, 0.004f,
+        x_max, y_max, 0.004f,
+        x_max, y_max - shiftY, 0.004f
     };
 
     userData->contVertices = malloc ( sizeof(GLfloat) * 24 );
@@ -363,11 +363,21 @@ int Init ( ESContext *esContext )
         0, 1, 2,
         2, 3, 0,
         4, 5, 6,
-        6, 7, 4
+        6, 7, 4,
     };
 
     userData->contIndices = malloc ( sizeof(GLuint) * 12 );
     memcpy(  userData->contIndices, contInd, sizeof(GLuint) * 12);
+
+    // for  (int i =0; i < 24; i++){
+    //     printf("\n contv %f", userData->contVertices[i]);
+    // }
+    // for  (int i =0; i < 16; i++){
+    //     printf("\n contt %f", userData->contTexCoords[i]);
+    // }
+    // for  (int i =0; i < 12; i++){
+    //     printf("\n conti %d", userData->contIndices[i]);
+    // }
     
     // координаты для текста ----
 
@@ -375,19 +385,19 @@ int Init ( ESContext *esContext )
     {
         blocksVerts[0 + 12*i] = (firstBlock + (float)(w/numBlocks * i));       // 0  x
         blocksVerts[1 + 12*i] = y_min + shiftY;                               //    y
-        blocksVerts[2 + 12*i] = 0.0004f; 
+        blocksVerts[2 + 12*i] = 0.004f; 
         
         blocksVerts[3 + 12*i] = (firstBlock + (float)(w/numBlocks * i));       // 1
         blocksVerts[4 + 12*i] = y_max - shiftY;
-        blocksVerts[5 + 12*i] = 0.0004f;
+        blocksVerts[5 + 12*i] = 0.004f;
 
         blocksVerts[6 + 12*i] = (float)(firstBlock + (w/numBlocks * (i + 1))); // 2
         blocksVerts[7 + 12*i] = y_max - shiftY;
-        blocksVerts[8 + 12*i] = 0.0004f;
+        blocksVerts[8 + 12*i] = 0.004f;
 
         blocksVerts[9 + 12*i] = (float)(firstBlock + (w/numBlocks * (i + 1))); // 3
         blocksVerts[10 + 12*i] = y_min + shiftY;
-        blocksVerts[11 + 12*i] = 0.0004f;
+        blocksVerts[11 + 12*i] = 0.004f;
     }
     
     userData->textVertices = malloc ( sizeof(GLfloat) * 3 * userData->numTextVertices );
@@ -412,7 +422,12 @@ int Init ( ESContext *esContext )
     
     userData->textIndices = malloc ( sizeof(GLuint) * userData->numTextIndices );
     memcpy( userData->textIndices, sqIndices, sizeof(GLuint) * userData->numTextIndices );
-
+    for  (int i =0; i < 48; i++){
+        printf("\n textv %f", userData->textVertices[i]);
+    }
+    for  (int i =0; i < 24; i++){
+        printf("\n texti %d", userData->textIndices[i]);
+    }
    //  //__________________ TEXT END ___________________
 
     userData->textureImage = createTexture("tex/backTexture.tga");
@@ -439,7 +454,7 @@ void Draw ( ESContext *esContext )
    GLfloat  colors[][4] = { 
        { 1.0f, 1.0f, 1.0f, 0.0f }, // nothing
        { 0.0f, 1.0f, 0.0f, 1.0f }, // green
-       { 0.0f, 0.0f, 0.0f, 0.0f }  //black
+       { 0.0f, 0.0f, 0.0f, 0.0f }, // black 0 - полностью прозрачный
    };
 
    //__________________ IMAGE ___________________
@@ -499,10 +514,14 @@ void Draw ( ESContext *esContext )
     
    
    
-    glActiveTexture ( GL_TEXTURE1 ); // текстурный блок 
+   glActiveTexture ( GL_TEXTURE1 ); // текстурный блок 
    glBindTexture ( GL_TEXTURE_2D, userData->textureBlack );
    glUniform1i ( userData->samplerLoc, 1 );
-    glVertexAttribPointer ( userData->positionLoc, 3, GL_FLOAT, 
+   
+   glUniform4fv( userData->pColorLoc, 1, colors[0]);
+   glUniform4fv( userData->uColorLoc, 1, colors[2]);
+
+   glVertexAttribPointer ( userData->positionLoc, 3, GL_FLOAT, 
                            GL_FALSE, 3 * sizeof(GLfloat), userData->contVertices );
    glVertexAttribPointer ( userData->texCoordLoc, 2, GL_FLOAT,
                             GL_FALSE, 2 * sizeof(GLfloat),  userData->contTexCoords);                        
@@ -512,10 +531,10 @@ void Draw ( ESContext *esContext )
 
    glUniformMatrix4fv( userData->mvpLoc, 1, GL_FALSE, (GLfloat*) &userData->mvpMatrix.m[0][0] );
 
-   //--------------------
+//    //--------------------
 
     glDrawElements( GL_TRIANGLES, 12, GL_UNSIGNED_INT, userData->contIndices );
-   //
+//    //
 
    glVertexAttribPointer ( userData->positionLoc, 3, GL_FLOAT, 
                            GL_FALSE, 3 * sizeof(GLfloat), userData->textVertices );
@@ -540,6 +559,10 @@ void Update ( ESContext *esContext, float deltaTime )
    ESMatrix modelview;
    float    aspect;
    getTexCoords( &userData->textTexCoords, userData->numTextVertices/*, text*/);
+//    for (int i = 0 ; i < userData->numTextVertices * 2; i++){
+//        printf("\n tex %d, %f", i, userData->textTexCoords[i]);
+//    }
+//    printf("____________________");
    // userData->angle += ( deltaTime * 40.0f );
    // if( userData->angle >= 360.0f )
    //    userData->angle -= 360.0f;
@@ -603,8 +626,8 @@ int updateNumber(int i, int sec)
 	buffer << setfill('0') << setw(4) << i;
 	const std::string tmp = buffer.str();
 	const char* cstr = tmp.c_str();
-	myIterator = (char*)malloc(10);
-	memcpy(myIterator, cstr, 10);
+	shared_I = (char*)malloc(10);
+	memcpy(shared_I, cstr, 10);
 
 	return 0;
 }
